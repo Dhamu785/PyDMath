508c88ea06eed1d5cc2003165dadcb31846ae100
Dhamu <dhamudhamodharan123@gmail.com>
2022-08-28 12:17:46 +0530

    s

--- pack/math.py
+++ pack/math.py
@@ -5,3 +5,12 @@ class perimeter:
 
     def rectangle(self,l,b):
         return 2*(l+b)
+    
+    def square(self,l):
+        return 4*l
+
+    def triangle(self,a,b,c):
+        return a+b+c
+    
+    def eq_triangle(self,a):
+        return 3*a
