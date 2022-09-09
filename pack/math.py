b6a8fe470db4492c7e388c6b2008ed147bc1f687
Dhamu785 <dhamodharan1888@gmail.com>
2022-09-09 14:30:26 +0530

    hemi sphere

--- pack/math.py
+++ pack/math.py
@@ -38,49 +38,72 @@ class perimeter:
 class area:
     pi = 22/7
     def circle(self,r):
+        # radius of circle
         return self.pi*(r**2)
 
     def rectangle(self,w,l):
+        # Length and width
         return w*l
 
     def square(self,a):
+        # Length of the sides
         return a**2
 
     def triangle(self,b,h):
+        # Bash and Height
         return (1/2)*(b*h)
     
     def eq_triangle(self,a):
+        # Length of all sides
         return (math.sqrt(3)/4)*(a**2)
 
     def parallelogram(self,b,h):
+        # bash and height
         return b*h
 
     def trapezium(self,a,b,h):
+        # opposite sides are equal
+        # h = Height
         num = (a+b)/2
         return num*h
 
     def isosceles_triangle(self,b,h):
+        # slanting heights are same 
+        # b = Base
         return (b*h)/2
 
 class volume:
     pi = 22/7
     def sphere(self,r):
+        # r = Radius
         return (4/3)*self.pi*(r**3)
 
     def cylinder(self,r,h):
+        # r = Radius
+        # h = Height
         return self.pi*(r**2)*h
 
     def cube(self,a):
+        # a = Length of four sides
         return a**3
 
     def rectangular_prism(self,l,b,h):
+        # l = length
+        # b = Base
+        # h = Height
         return l*b*h
 
     def cone(self,r,h):
+        # r = Radius
+        # h = Height
         return (1/3)*self.pi*(r**2)*h
 
     def right_rectangular_pyramid(self,l,w,h):
-        """           l = length 
-           w = width
-           h = height"""
+        # l = length 
+        # w = width
+        # h = height
         return (l*w*h)/3
+
+    def hemi_sphere(self,r):
+        # r = Radius of sphere
+        return (2/3)*self.pi*(r**3)
