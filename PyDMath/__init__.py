3136039fd782996180c494c82e8eadaa3e0f27cf
Dhamu <dhamudhamodharan123@gmail.com>
2022-09-19 23:05:25 +0530

    LCM

--- PyDMath/__init__.py
+++ PyDMath/__init__.py
@@ -1,3 +1,3 @@
 from .mensuration import perimeter,area,volume
 from .per_and_comb import fact,permutation,combination
-from .lcm_andhcf import lcm,hcf
+from .lcm_andhcf import hcf,lcm
--- PyDMath/lcm_andhcf.py
+++ PyDMath/lcm_andhcf.py
@@ -1,12 +1,21 @@
-def hcf(*kwargs):
+def hcf(*args):
     q = []
-    for i in kwargs:
+    for i in args:
         q.append(i)
     q.sort()
     if len(q) == 2:
         for j in range(q[0],0,-1):
             if (int(q[0]) % j == 0) and (int(q[1]) % j == 0):
-                print(j)
-                break
+                return j
+            
 
-hcf(45,63)
+def lcm(*args):
+    hcf1 = hcf(*args)
+    r = []
+    for i in args:
+        r.append(int(i))
+    if len(r) == 2:
+        lcm1 = (r[0]*r[1]) / int(hcf1)
+        print(lcm1)
+
+lcm(18,15)
