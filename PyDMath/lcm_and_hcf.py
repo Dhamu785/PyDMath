51b218af09355dae4d26d0fa0b60bdf97f7900fa
Dhamu <dhamudhamodharan123@gmail.com>
2022-09-19 23:55:55 +0530

    Three digits

--- PyDMath/lcm_and_hcf.py
+++ PyDMath/lcm_and_hcf.py
@@ -1,12 +1,22 @@
+import numpy as np
 def hcf(*args):
     q = []
     for i in args:
-        q.append(i)
+        try:
+            q.append(int(i))
+        except:
+            return 'It requires only numbers'
     q.sort()
     if len(q) == 2:
         for j in range(q[0],0,-1):
-            if (int(q[0]) % j == 0) and (int(q[1]) % j == 0):
+            if (q[0] % j == 0) and (q[1] % j == 0):
                 return j
+    elif len(q) == 3:
+        for j in range(q[0],0,-1):
+            if (q[0]%j == 0) and (q[1]%j == 0) and (q[2]% j == 0):
+                return j
+    else:
+        return 'It accepts only 5 values'
             
 
 def lcm(*args):
@@ -15,7 +25,8 @@ def lcm(*args):
     for i in args:
         r.append(int(i))
     if len(r) == 2:
-        lcm1 = (r[0]*r[1]) / int(hcf1)
-        print(lcm1)
+        lcm1 = np.product(r) / int(hcf1)
+        return lcm1
 
-lcm(18,15)
+print(hcf(4,6))
+print(lcm(4,6))
