c34681fc663e3411a2a9ce84a7bdcf363cdca188
Dhamu <dhamudhamodharan123@gmail.com>
2022-09-20 22:25:30 +0530

    Upto 4 values

--- PyDMath/lcm_and_hcf.py
+++ PyDMath/lcm_and_hcf.py
@@ -22,7 +22,7 @@ def hcf(*args):
                 return j
             
     else:
-        return 'It accepts only 5 values'
+        return 'It accepts only 4 values'
             
 
 def lcm(*args):
@@ -33,8 +33,8 @@ def lcm(*args):
     r.sort()
     if len(r) == 2:
         lcm1 = np.product(r) / int(hcf1)
-        return lcm1
-    if len(r) == 3:
+        return int(lcm1)
+    elif len(r) == 3:
         x = []
         y = []
         z = []
@@ -50,3 +50,25 @@ def lcm(*args):
                     return j
             pre_extend = extend
             extend = extend+5
+        
+    elif len(r) == 4:
+        w = []
+        x = []
+        y = []
+        z = []
+        pre_extend = 1
+        extend = 5
+        while True:
+            for i in range(pre_extend,extend):
+                w.append(r[0]*i)
+                x.append(r[1]*i)
+                y.append(r[2]*i)
+                z.append(r[3]*i)
+            for j in w:
+                if (j in x) and (j in y) and (j in z):
+                    return j
+            pre_extend = extend
+            extend = extend+5
+
+    else:
+        return 'It accepts only 4 values'
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import PyDMath
-a= PyDMath.lcm(5,8,15)
+a= PyDMath.lcm(5,8,15,23)
 print(a)
