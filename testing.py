2f9dbe739b6e719dce0b5da052997f873735b5d6
Dhamu <dhamudhamodharan123@gmail.com>
2022-09-25 11:42:45 +0530

    test

--- testing.py
+++ testing.py
@@ -8,7 +8,7 @@
 def calc(li):
     prime_no = [2,3,5,7,11,13,17,19,23,29,31,37,41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
     prime_factors = []
-    while li[0] >= 1.0 and li[1] >= 1.0:
+    while li[0] > 1.0 and li[1] > 1.0:
         for i in prime_no:
             count = 0
             if li[0]%i == 0:
