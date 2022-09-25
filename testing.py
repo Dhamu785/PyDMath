2a73b1ddd7d46677b1e35a0dc63525cab5f4ce8e
Sabari2358 <1888sabari@gmail.com>
2022-09-25 23:10:19 +0530

    3 three dight lcm

--- testing.py
+++ testing.py
@@ -5,7 +5,9 @@
 # # z = input()
 # print(a.circle(x))
 
-def calc(li):
+
+import math
+def lcm2(li):
     prime_no = [2,3,5,7,11,13,17,19,23,29,31,37,41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
     prime_factors = []
     while li[0] > 1.0 or li[1] > 1.0:
@@ -20,6 +22,25 @@ def calc(li):
             if count >= 1:
                 prime_factors.append(i)
                 break
-    print(prime_factors)
-    print(li)
-calc([8,13])
+    print(math.prod(prime_factors))
+
+def lcm3(li):
+    prime_no = [2,3,5,7,11,13,17,19,23,29,31,37,41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    prime_factors = []
+    while li[0] > 1.0 or li[1] > 1.0 or li[2] > 1:
+        for i in prime_no:
+            count = 0
+            if li[0]%i == 0:
+                li[0] = li[0]/i
+                count += 1
+            if li[1]%i == 0:
+                li[1] = li[1]/i
+                count += 1
+            if li[2]%i == 0:
+                li[2] = li[2]/i
+                count += 1
+            if count >= 1:
+                prime_factors.append(i)
+                break
+    print(math.prod(prime_factors))
+lcm3([15,17,89])
