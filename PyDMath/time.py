3d674b62743413f73fd9e4f699dad701693ac922
Dhamu <dhamudhamodharan123@gmail.com>
2022-10-14 22:36:12 +0530

    year code done

--- PyDMath/time.py
+++ PyDMath/time.py
@@ -22,7 +22,6 @@ class year:
     
     def year(self,y):
         year = int(y) - int(y[2:])
-        print(int(y[2:]))
         yr = None
         if str(year/400)[-2:] == '25':
             yr = 5
@@ -36,10 +35,28 @@ class year:
             yr = 'Error'
         return yr
 
+    def year_diff(self,yrs):
+        if int(yrs[2:]) == 0:
+            return 0
+        else:
+            diff = int(yrs[2:])
+            leap_yr = 0
+            nml_yr = 0
+            for i in range(1,diff+1):
+                if i%4 == 0:
+                    leap_yr += 1
+                else:
+                    nml_yr += 1
+            return leap_yr*2 + nml_yr
+
     def find_day(self,date,month,year):
+        # year calculation
         yr = self.year(str(int(year)-1))
 
         # year difference
+        yrs = self.year_diff(str(int(year)-1))
+
+        # Month calculation
         month_days = [3,1,3,2,3,2,3,3,2,3,2,3]
         if self.leap_year(year) == 'Leap year':
             month_days[1] = 1
@@ -49,4 +66,9 @@ class year:
         for i in range(0,int(month)-1):
             month_total_days = month_total_days + month_days[i]
 
-        return yr + (month_total_days%7) + (int(date))%7
+        print('year:',yr)
+        print('diff',yrs%7)
+        print('month',month_total_days)
+        print('day',int(date)%7)
+
+        return yr + (month_total_days%7) + (int(date))%7 + yrs%7
