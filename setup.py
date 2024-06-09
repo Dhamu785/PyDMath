84bee357a5c0adcf7d5dccd7260e2c87a2a77b09
dhamodharan <dhamodharan1888@gmail.com>
2024-06-09 18:21:48 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+
+VERSION = '0.1'
+DESCRIPTION = 'Solving the maths problems'
+LONG_DESCRIPTION = 'A package that allows to solve fundamental math problems.'
+
+# Setting up
+setup(
+    name="PyDMath",
+    version=VERSION,
+    author="Dhamodharan",
+    author_email="dhamodharan1888.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['math', 'numpy'],
+    keywords=['python', 'math', 'problems', 'mathematics', 'scientific calculations'],
+    classifiers=[
+        "Development Status :: 1 - Development",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
